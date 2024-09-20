1c54e744605182c1fc2541450d32574bf14356ce
DANIEL DE ARAUJO ALVES <106118112+araujoalves@users.noreply.github.com>
2024-09-20 16:41:38 -0300

    Delete Projeto de Importação e Processamento de Dados do Kaggle​ directory

--- Projeto de Importação e Processamento de Dados do Kaggle​/Arquivo_CSV..py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# Leitura do arquivo CSV baixado
-data = pd.read_csv('netflix_data/netflix_titles.csv')  # Caminho do arquivo CSV baixado
-
-print("Dados lidos com sucesso do arquivo CSV!")
